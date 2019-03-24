016a283983219bee7119aa4c3e365c3c7dd50034
fOURP92 <panos9229@gmail.com>
2019-03-24 21:09:50 +0200

    ubuntu test1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,10 @@
-a = 3
-b = 5
-
 for x in range(100):
-    if x % a == 0 and x % b == 0:
+    if x % 3 == 0 and x % 5 == 0:
         print("fizzbuzz")
         
-    elif x % a == 0:
+    elif x % 3 == 0:
          print("fizz")
-    elif x % b == 0:
+    elif x % 5 == 0:
         print("buzz")
 
     else:
